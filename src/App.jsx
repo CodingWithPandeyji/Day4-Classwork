9292e4d8008ecabb0b2e9150bc08f086e8a73f00
CodingWithPandeyji <mohitpandey.cs19@gmail.com>
2024-06-01 20:04:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Quote from './Components/Quote'
-import Nav from './Components/Nav'
-import './App.css'
+import React from "react";
+import Quote from "./Components/Quote";
+import Nav from "./Components/Nav";
+import "./App.css";
+import data from "./data/Data";
+
 function App() {
   return (
     <>
-   <Nav/>
-   <Quote/>
- </>
+      <Nav />
+      {
+        data.map((item) => {
+          return <Quote content={item.quote} auth={item.author} />
+        })
+      }
+    </>
   )
 }
 
-export default App
+export default App;
--- src/Components/Quote.jsx
+++ src/Components/Quote.jsx
@@ -1,48 +1,14 @@
 import React from 'react'
 import './quote.css'
 
-function Quote() {
-    const data=[
-        {"quote": "The only limit to our realization of tomorrow is our doubts of today.", "author": "Franklin D. Roosevelt"},
-    {"quote": "The purpose of our lives is to be happy.", "author": "Dalai Lama"},
-    {"quote": "Life is what happens when you're busy making other plans.", "author": "John Lennon"},
-    {"quote": "Get busy living or get busy dying.", "author": "Stephen King"},
-    {"quote": "You have within you right now, everything you need to deal with whatever the world can throw at you.", "author": "Brian Tracy"},
-    {"quote": "Believe you can and you're halfway there.", "author": "Theodore Roosevelt"},
-    {"quote": "The only impossible journey is the one you never begin.", "author": "Tony Robbins"},
-    {"quote": "Act as if what you do makes a difference. It does.", "author": "William James"},
-    {"quote": "Success is not how high you have climbed, but how you make a positive difference to the world.", "author": "Roy T. Bennett"},
-    {"quote": "The best way to predict the future is to invent it.", "author": "Alan Kay"},
-    {"quote": "It is never too late to be what you might have been.", "author": "George Eliot"},
-    {"quote": "Your time is limited, don't waste it living someone else's life.", "author": "Steve Jobs"},
-    {"quote": "The mind is everything. What you think you become.", "author": "Buddha"},
-    {"quote": "Life is really simple, but we insist on making it complicated.", "author": "Confucius"},
-    {"quote": "Turn your wounds into wisdom.", "author": "Oprah Winfrey"},
-    {"quote": "Don't watch the clock; do what it does. Keep going.", "author": "Sam Levenson"},
-    {"quote": "If you want to lift yourself up, lift up someone else.", "author": "Booker T. Washington"},
-    {"quote": "The only way to do great work is to love what you do.", "author": "Steve Jobs"},
-    {"quote": "The best way to find yourself is to lose yourself in the service of others.", "author": "Mahatma Gandhi"},
-    {"quote": "Happiness is not something ready made. It comes from your own actions.", "author": "Dalai Lama"},
-    {"quote": "What we think, we become.", "author": "Buddha"},
-    {"quote": "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.", "author": "Christian D. Larson"},
-    {"quote": "Don't let yesterday take up too much of today.", "author": "Will Rogers"},
-    {"quote": "Change your thoughts and you change your world.", "author": "Norman Vincent Peale"},
-    {"quote": "Life itself is the most wonderful fairy tale.", "author": "Hans Christian Andersen"},
-    {"quote": "You must be the change you wish to see in the world.", "author": "Mahatma Gandhi"},
-    {"quote": "Keep your face always toward the sunshine—and shadows will fall behind you.", "author": "Walt Whitman"},
-    {"quote": "The only person you are destined to become is the person you decide to be.", "author": "Ralph Waldo Emerson"},
-    {"quote": "May you live all the days of your life.", "author": "Jonathan Swift"},
-    {"quote": "To live is the rarest thing in the world. Most people exist, that is all.", "author": "Oscar Wilde"} 
-]
+function Quote({content,auth}){
   return (
     
-   data.map((ele,index)=>(
-    <div className='quotes' key={index}>
-        <p>{ele.quote}</p>
-        <h3>{ele.author}</h3>
+ 
+    <div className='quotes' >
+        <p>{content}</p>
+        <h3>{auth}</h3>
     </div>
-   ))
-
    
   )
 }
